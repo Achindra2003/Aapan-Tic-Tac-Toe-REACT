2e63260bbc554a43449528174b12a3224e2ca0f0
Achindra2003 <138213900+Achindra2003@users.noreply.github.com>
2023-07-31 13:03:23 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,19 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        An
+        <a href="">Aapan Log</a> Project
+      </p>
+
+      <p>
+      Created by
+        <a href="" style="color: var(--red)"
+          >Achindra Sharma</a
+        >
+      </p>
+    </footer>
+  );
+}
